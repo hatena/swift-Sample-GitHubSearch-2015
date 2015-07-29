c3cde85112ab35b0e7f9a69fd840ea95e1cc1e42
cockscomb <cockscomb@hatena.ne.jp>
2015-07-29 18:12:20 +0900

    APIClient

---
+++ GitHubSearch/GitHub.swift
@@ -0,0 +1,124 @@
+//
+//  GitHub.swift
+//  GitHubSearch
+//
+//  Created by Hiroki Kato on 2015/07/29.
+//  Copyright © 2015年 Hatena Co., Ltd. All rights reserved.
+//
+
+import Foundation
+
+import AFNetworking
+
+public typealias JSONObject = [String: AnyObject]
+
+public enum HTTPMethod {
+    case Get
+}
+
+public protocol APIEndpoint {
+    var path: String { get }
+    var method: HTTPMethod { get }
+    var parameters: [NSObject: AnyObject] { get }
+    typealias ResponseType: JSONDecodable
+}
+
+public enum APIError: ErrorType {
+    case UnexpectedResponse
+}
+
+public class GitHubAPI {
+    private let HTTPSessionManager: AFHTTPSessionManager = {
+        let manager = AFHTTPSessionManager(baseURL: NSURL(string: "https://api.github.com/"))
+        manager.requestSerializer.setValue("application/vnd.github.v3+json", forHTTPHeaderField: "Accept")
+        return manager
+        }()
+    
+    public func request<Endpoint: APIEndpoint>(endpoint: Endpoint, handler: (task: NSURLSessionDataTask, response: Endpoint.ResponseType?, error: ErrorType?) -> Void) {
+        let success = { (task: NSURLSessionDataTask!, response: AnyObject!) -> Void in
+            if let JSON = response as? JSONObject {
+                do {
+                    let response = try Endpoint.ResponseType(JSON: JSON)
+                    handler(task: task, response: response, error: nil)
+                } catch {
+                    handler(task: task, response: nil, error: error)
+                }
+            } else {
+                handler(task: task, response: nil, error: APIError.UnexpectedResponse)
+            }
+        }
+        let failure = { (task: NSURLSessionDataTask!, error: NSError!) -> Void in
+            handler(task: task, response: nil, error: error)
+        }
+        
+        switch endpoint.method {
+        case .Get:
+            HTTPSessionManager.GET(endpoint.path, parameters: endpoint.parameters, success: success, failure: failure)
+        }
+    }
+    
+    // MARK: - Endpoints
+    
+    public struct SearchRepositories: APIEndpoint {
+        public var path = "search/repositories"
+        public var method = HTTPMethod.Get
+        public var parameters: [NSObject: AnyObject] {
+            return [:]
+        }
+        public typealias ResponseType = SearchResult<Repository>
+    }
+}
+
+public protocol JSONDecodable {
+    init(JSON: JSONObject) throws
+}
+
+public enum JSONDecodeError: ErrorType {
+    case MissingRequiredKey(String)
+    case UnexpectedType(key: String, expected: Any.Type, actual: Any.Type)
+}
+
+public struct SearchResult<ItemType: JSONDecodable>: JSONDecodable {
+    let totalCount: Int
+    let incompleteResults: Bool
+    let items: [ItemType]
+    
+    public init(JSON: JSONObject) throws {
+        self.totalCount = try getValue(JSON, key: "total_count")
+        self.incompleteResults = try getValue(JSON, key: "incomplete_results")
+        self.items = try (getValue(JSON, key: "items") as [JSONObject]).mapWithRethrow { return try ItemType(JSON: $0) }
+    }
+}
+
+public struct Repository: JSONDecodable {
+    let id: Int
+    let name: String
+    
+    public init(JSON: JSONObject) throws {
+        self.id = try getValue(JSON, key: "id")
+        self.name = try getValue(JSON, key: "name")
+    }
+}
+
+
+// MARK: - Utilities
+
+private func getValue<T>(JSON: JSONObject, key: String) throws -> T {
+    guard let value = JSON[key] else {
+        throw JSONDecodeError.MissingRequiredKey(key)
+    }
+    guard let typedValue = value as? T else {
+        throw JSONDecodeError.UnexpectedType(key: key, expected: T.self, actual: value.dynamicType)
+    }
+    return typedValue
+}
+
+private extension Array {
+    func mapWithRethrow<T>(@noescape transform: (Array.Generator.Element) throws -> T) rethrows -> [T] {
+        var mapped: [T] = []
+        for element in self {
+            mapped.append(try transform(element))
+        }
+        return mapped
+    }
+}
